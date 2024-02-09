608a82cf9ffeede06c39c17855cfa171db3614a2
dalaaal <159532677+dalaaal@users.noreply.github.com>
2024-02-09 11:38:22 +0000

    Add files via upload

---
+++ etudiant.java
@@ -0,0 +1,13 @@
+
+public class etudiant {
+
+	public etudiant() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
